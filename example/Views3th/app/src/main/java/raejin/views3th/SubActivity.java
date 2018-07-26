f546fa93a2d96787a8febbaa49f7e580ec1f4bd0
Unknown <jyeon86@gmail.com>
2018-07-26 17:20:31 +0900

    앱프로그래밍 0726

---
+++ example/Views3th/app/src/main/java/raejin/views3th/SubActivity.java
@@ -0,0 +1,22 @@
+package raejin.views3th;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+    TextView textView_sub;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        textView_sub = (TextView)findViewById(R.id.textView_sub);
+        textView_sub.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
+}
